1b0bd92b785aed0b4a7fdca52c2ab6b366120d7e
niltonsSantana-devBa <nilton.s.santana@ba.estudante.senai.br>
2025-10-03 21:58:02 -0300

    Feat: cadastro do cliente comentarios

--- clientes/cadastrar.py
+++ clientes/cadastrar.py
@@ -1,3 +1,9 @@
 # Arquivo para a funcionalidade de 'cadastrar'
 k = input("Digite o nome do cliente: ")
 print(f"Cliente {k} cadastrado com sucesso!")
+# IGNORE - apenas para teste de commit      
+# Feat: cadastro do cliente
+# Fix: correção de bug no cadastro
+# Refactor: melhoria no código de cadastro  
+# Docs: atualização da documentação do cadastro
+# Test: adição de testes para o cadastro
